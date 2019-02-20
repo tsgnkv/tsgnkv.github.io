c8c623f2a4c20c4c8ced556a55913ede5f611289
tsgnkv <atsygankov@hotmail.com>
2019-02-20 11:34:27 +0300

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -12,16 +12,9 @@ firebase.initializeApp({
 Retrieve an instance of Firebase Messaging so that it can handle background messages.
 */
 const messaging = firebase.messaging()
+
 messaging.setBackgroundMessageHandler(function (payload) {
     console.log('[firebase-messaging-sw.js] Received background message ', payload);
-    const notification = JSON.parse(payload.data.notification);
-    // Customize notification here
-    const notificationTitle = notification.title;
-    const notificationOptions = {
-        body: notification.body,
-        icon: notification.icon
-    };
-
-    return self.registration.showNotification(notificationTitle,
-        notificationOptions);
+    payload.data.data = JSON.parse(JSON.stringify(payload.data));
+    registration.showNotification(payload.data.title, payload.data);
 });
